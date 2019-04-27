453f536651e8ad71604aa2ed6e68ac981e64f609
SeAmenLoo <1742914039@qq.com>
2019-04-27 22:31:35 +0800

    pondnd_new_model

---
+++ Assets/Scripts/TFObject/Obstacle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : TFObject{
+    bool gameOver = false;
+    public delegate void GameOver(int i);
+    public static event GameOver GameOverHandler;
+    protected override void OnCollisionWithPlayer()
+    {
+        if (!gameOver) {
+            GameOverHandler(0);
+            gameOver = true;
+        }
+            
+    }
+
+    protected override void OnCollisionWithWebNode()
+    {
+        if (!gameOver)
+        {
+            GameOverHandler(1);
+            gameOver = true;
+        }
+    }
+
+    protected override void OnCollisionWithWebPole()
+    {
+        if (!gameOver)
+        {
+            GameOverHandler(0);
+            gameOver = true;
+        }
+    }
+
+    public void SetObstacle(float speed) {
+        base.SetSpeed(speed);
+    }
+
+    private void FixedUpdate()
+    {
+        SetSpeed(PathManager.GetCurSpeed());
+        transform.Translate(Vector3.back * _speed);
+        base.DestroyWhenOutofMap();
+    }
+
+    private void Awake()
+    {
+       SetObstacle(PathManager.curspeed);
+    }
+
+}
